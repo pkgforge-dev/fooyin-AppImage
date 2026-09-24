fa20b9866eb96c4dffe06265d1d2d83ed5f0b4bc
Link4Electronics <link4electronics@gmail.com>
2026-09-24 05:36:50 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    cmake
     fooyin              \
     lxqt-qtplugin       \
     kdsingleapplication \
@@ -27,3 +28,25 @@ get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini libdecor-mini
 #make-aur-package fooyin
 #make-aur-package fooyin-plugin-libvgm-git
 #make-aur-package fooyin-plugin-msuinput-git
+echo "Building fooyin-plugin-libvgm..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/fooyin/fooyin-plugin-libvgm"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --recursive --depth 1 "$REPO" ./fooyin-plugin-libvgm
+echo "$VERSION" > ~/version
+
+cmake -B build -S ./fooyin-plugin-libvgm -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build build -j$(nproc)
+cmake --install build
+
+echo "Building fooyin-plugin-msuinput..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/Vo1dTear/fooyin-plugin-msuinput"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./fooyin-plugin-msuinput
+echo "$VERSION" > ~/version
+
+cmake -B build -S ./fooyin-plugin-msuinput -DCMAKE_BUILD_TYPE=Release
+cmake --build build -j$(nproc)
+cmake --install build
+
