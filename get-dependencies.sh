63996656b7d97b9aaf0d15bc9c6db6eb62242943
Link4Electronics <link4electronics@gmail.com>
2026-09-24 05:39:09 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,7 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake
+    cmake               \
     fooyin              \
     lxqt-qtplugin       \
     kdsingleapplication \
